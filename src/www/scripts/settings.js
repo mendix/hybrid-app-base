351529727659b1b1658ceb1b097ccea0237f2d75
Kevin Vlaanderen <kevin.vlaanderen@mendix.com>
2017-05-10 12:21:45 +0200

    Fix WkWebview bug > Enable WkWebview by default, remove WkWebview flag

--- src/www/scripts/settings.js
+++ src/www/scripts/settings.js
@@ -5,7 +5,7 @@ module.exports = (function() {
         request.overrideMimeType("application/json");
         request.open('GET', file, true); // Replace 'my_data' with the path to your file
         request.onreadystatechange = function () {
-            if (request.readyState == 4 && request.status == "200") {
+            if (request.readyState == 4) {
                 // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
                 callback(request.responseText);
             }
