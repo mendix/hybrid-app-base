c27304014900d6bba20da5922233e85f79b7b029
Kevin Vlaanderen <kevin.vlaanderen@mendix.com>
2017-10-11 18:08:33 +0200

    Remove all occurences of profile settings

--- src/www/scripts/entry.js
+++ src/www/scripts/entry.js
@@ -5,7 +5,7 @@ var Settings = require("./settings");
 document.addEventListener("deviceready", function() {
     Settings.loadJSON("settings.json", function (response) {
         var settings = JSON.parse(response);
-        MxApp.initialize(settings.url, settings.hybridTabletProfile, settings.hybridPhoneProfile,
+        MxApp.initialize(settings.url,
             settings.enableOffline, settings.requirePin,
             settings.username, settings.password);
     });
