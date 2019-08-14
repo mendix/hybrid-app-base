e77e1db04b953c6acbc58a35c3203617f3d834b3
Jeremiah Ogbomo <jeremiah.ogbomo@mendix.com>
2019-08-14 15:51:34 +0200

    Handle unsecured android devices

--- src/www/scripts/secure-store.js
+++ src/www/scripts/secure-store.js
@@ -6,9 +6,28 @@ module.exports = (function() {
     var getStorage = function(namespace) {
         if (!namespacePromises[namespace]) {
             namespacePromises[namespace] = new Promise(function (resolve, reject) {
-                var storage = new cordova.plugins.SecureStorage(function () {
-                    resolve(storage);
-                }, reject, namespace);
+                var storage = new cordova.plugins.SecureStorage(
+                    function () {
+                        resolve(storage);
+                    },
+                    function(error) {
+                        if (error.message === "Device is not secure") {
+                            var retry = function () {
+                                resolve(getStorage());
+                            }
+                            return navigator.notification.alert(
+                                "Please enable the screen lock on your device. This app cannot operate securely without it.",
+                                function () {
+                                    storage.secureDevice(retry, retry);
+                                },
+                                "Screen lock is disabled"
+                            );
+                        }
+            
+                        reject(e);
+                    },
+                    namespace
+                );
             });
         }
 
