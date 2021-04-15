7184df714e1c2265ac505b1d33ecb247fb2d0271
Stefanos Markidis <stmarkidis@gmail.com>
2021-04-15 19:56:27 +0200

    Update more webpack and babel dependencies

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,24 +1,24 @@
-var fs = require("fs");
-var path = require("path");
+const fs = require("fs");
+const path = require("path");
 
-var CopyWebpackPlugin = require("copy-webpack-plugin");
-var CleanWebpackPlugin = require('clean-webpack-plugin');
-var I18nPlugin = require("i18n-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const I18nPlugin = require("@zainulbr/i18n-webpack-plugin");
 
-var utils = require("./utils");
+const utils = require("./utils");
 
-module.exports = function(env) {
+module.exports = function () {
     // Prepare the dist directories. It has to exist before we attempt to create the .zip archive.
     if (!fs.existsSync("dist")) {
         fs.mkdirSync("dist");
     }
-
-    var default_splash_path = utils.getBaseOrCustomPath("src/resources/splash.png");
+    const default_splash_path = utils.getBaseOrCustomPath("src/resources/splash.png");
+    const default_resources_path = utils.getBaseOrCustomPath("src/www/resources.zip");
     var styling_path = utils.getBaseOrCustomPath("src/www/styles/");
-    var default_resources_path = utils.getBaseOrCustomPath("src/www/resources.zip");
 
     // Build the configuration object.
-    var config = {
+    return {
+        mode: "development",
         // The starting point of our app
         entry: {
             bundle: utils.getBaseOrCustomPath("src/www/scripts/entry.js")
@@ -46,12 +46,12 @@ module.exports = function(env) {
                     use: {
                         loader: 'babel-loader',
                         options: {
-                            presets: [['env', {
-                                "targets": {
-                                    "uglify": true
-                                }
-                            }]],
-                            plugins: ["transform-regenerator"],
+                            presets: [
+                                ['@babel/preset-env', {
+                                    targets: "> 0.25%, not dead",
+                                }]
+                            ],
+                            plugins: ["@babel/transform-regenerator"],
                             cacheDirectory: true
                         }
                     }
@@ -69,35 +69,37 @@ module.exports = function(env) {
             new I18nPlugin(utils.loadConfiguration("config/texts.json"), {
                 hideMessage: true
             }),
-            new CopyWebpackPlugin( // Image files
-                utils.getBaseAndCustomPaths("src/www/images").map(function(dir) {
-                    return {
-                        context: dir,
-                        from: "**/*",
-                        to: path.normalize("www/img")
-                    }
-                })
-            ),
-            new CopyWebpackPlugin([
-                {
-                    context: path.dirname(default_splash_path),
-                    from: path.basename(default_splash_path),
-                    to: path.basename(default_splash_path)
-                },
-                {
-                    context: path.dirname(styling_path),
-                    from: '**/*.css',
-                    to: path.normalize("www/css/[name].css")
-                },
-                ...(fs.existsSync(default_resources_path) ?
-                        [{
-                            context: path.dirname(default_resources_path),
-                            from: path.basename(default_resources_path),
-                            to: path.normalize("www/resources.zip")
-                        }] : []),
-            ])
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        context: path.dirname(default_splash_path),
+                        from: path.basename(default_splash_path),
+                        to: path.basename(default_splash_path),
+                        noErrorOnMissing: true
+
+                    },
+                    {
+                        context: path.dirname(default_resources_path),
+                        from: path.basename(default_resources_path),
+                        to: path.normalize("www/resources.zip"),
+                        noErrorOnMissing: true
+                    },
+                    {
+                        context: path.dirname(styling_path),
+                        from: '**/*.css',
+                        to: path.normalize("www/css/[name].css"),
+                        noErrorOnMissing: true
+                    },
+                    ...utils.getBaseAndCustomPaths("src/www/images").map(function (dir) {
+                        return {
+                            context: dir,
+                            from: "**/*",
+                            to: path.normalize("www/img"),
+                            noErrorOnMissing: true
+                        };
+                    })
+                ]
+            })
         ]
     };
-
-    return config;
 };
